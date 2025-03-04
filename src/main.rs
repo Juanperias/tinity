962b37cb09434126c4e4d902868b0fdb37f2b2a8
Juanperias <136520331+Juanperias@users.noreply.github.com>
2025-03-04 16:38:39 -0400

    refactor: use Result to replace .unwrap

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,30 @@ mod riscv;
 
 use crate::parser::token::get_tokens;
 use binary::{elf::Elf, Binary, Section};
-use object::write::Object;
+use anyhow::Result;
 use object::{Architecture, Endianness};
-use tracing::info;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
-fn main() {
+fn main() -> Result<()> {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
         .finish();
 
-    tracing::subscriber::set_global_default(subscriber).expect("Cannot set default subscriber");
+    tracing::subscriber::set_global_default(subscriber)?;
 
-    let input = std::fs::read_to_string("test.tir").unwrap();
+    let input = std::fs::read_to_string("test.tir")?;
     let tokens = get_tokens(input);
     println!("{:?}", tokens);
 
     let mut elf = Elf::new(Architecture::Riscv64, Endianness::Little);
 
-    let mut f = std::fs::File::create("output.elf").unwrap();
+    let mut f = std::fs::File::create("output.elf")?;
 
     elf.create_section(Section::Text);
     elf.write_section(Section::Text, vec![]);
 
     elf.save(&mut f).unwrap();
+
+    Ok(())
 }
